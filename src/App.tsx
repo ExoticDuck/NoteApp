21c0496302b51bee1bfbb8769e36c7e9e30c2a9e
ExoticDuck <marchenkov.n@inbox.ru>
2022-03-24 19:48:07 +0300

    test

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ function App() {
   return (
     <div className="App">
       <h1>App</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere aliquid at aut enim ducimus aliquam debitis accusantium fugiat. Quidem architecto illo maiores quae vel id quis tempore eius, quas laudantium natus quaerat? Nisi quisquam expedita sint vero, nihil, minus esse enim recusandae a ratione molestiae dignissimos, temporibus consequuntur aliquid voluptate.</p>
     </div>
   );
 }
